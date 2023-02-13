dfb40207554bd385bb02508a8c446a3e456c6e13
Ajay <Ajay4uu.ak@gmail.com>
2023-02-13 12:42:55 +0530

    commit for docker registery

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:alpine
+
+WORKDIR /usr/app
+
+COPY . .
+
+RUN npm install
+
+EXPOSE 4000
+
+CMD ["npm","start"]
+
---
+++ index.js
@@ -0,0 +1,19 @@
+let express = require("express")
+
+let app = express()
+
+app.get("/",(req,res)=>{
+    res.send("this is home page")
+})
+
+app.get("/signup",(req,res)=>{
+    res.send("this is signup page")
+})
+
+// app.get("/logout",(req,res)=>{
+//     res.send("this is logout file")
+// })
+
+app.listen(4000,()=>{
+    console.log("server is running at port 4000")
+})
